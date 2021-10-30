e807a220ea871b3d652d180cca2f70f788a9ec97
Uhunghama Harrison <engrbrain@gmail.com>
2021-10-30 16:10:20 +0100

    reinstall nodemon after an error

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express();
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send("Hello API !");
-})
+});
 app.listen(3000, () => {
      console.log('Server is running http://localhost:3000');
 });
