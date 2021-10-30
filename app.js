f26980b06a8a89fab4503d226c77b827419e5290
Uhunghama Harrison <engrbrain@gmail.com>
2021-10-30 20:18:37 +0100

    Analysing the E-Shop Database

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ require('dotenv/config');
 const api = process.env.API_URL;
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+
+
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
+//End of middleware
 
+//connect to database
 mongoose.connect(process.env.CONNECTION_STRING, 
     {
         useNewUrlParser: true, 
@@ -19,23 +23,57 @@ mongoose.connect(process.env.CONNECTION_STRING,
 }).catch((err)=> {
     console.log(err);
 })
+// End of Connect to Database
 
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some_url'
-    };
-    res.send(product);
+
+
+//Create database model for product
+
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: {
+        type: Number,
+        required: true
+    }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+//End of create database models
+
+//make API requests POST, PUT, GET, DELETE
+app.get(`${api}/products`, async (req, res) => {
+    const productList = await Product.find();
+
+    if(!productList) {
+        res.status(500).json({success:false})
+    }
+    res.send(productList);
 });
 
 app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    });
+    product.save().then((createdProduct =>{
+        res.status(201).json(createdProduct)
+    })).catch((err)=>{
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
+    
 });
 
+// End of make API Requests
+
+// Setup Node Server
 app.listen(3000, () => {
-    console.log(api);
+    //console.log(api);
      console.log('Server is running http://localhost:3000');
 });
+// End of Setup Node Server
