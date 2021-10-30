04baaf6ee6fe703e4edfbd73545fabf53dda6331
Uhunghama Harrison <engrbrain@gmail.com>
2021-10-30 17:53:34 +0100

    Create First API Call & Parsing Json Data

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express();
-
+const bodyParser = require('body-parser');
 require('dotenv/config');
 const api = process.env.API_URL;
 
-app.get(api + '/products', (req, res) => {
-    res.send("Hello API !");
+//middleware
+app.use(bodyParser.json());
+
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_url'
+    };
+    res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
 });
 
 app.listen(3000, () => {
